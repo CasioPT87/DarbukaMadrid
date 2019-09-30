19b9538ae74c93471f64ba136e251d8a0e9dddd2
CasioPT87 <sergioibanezmoreno@gmail.com>
2019-09-30 21:39:36 +0100

    intento de cambiar el favicob

--- src/assets/texts/homeContacts.js
+++ src/assets/texts/homeContacts.js
@@ -6,7 +6,7 @@ export default {
     Una de las razones para que reconozcamos un estilo musical, es la ornamentación, en percusión se llaman rudimentos algunos ejemplos son flangs, diddle, paradiddle.
     La darbuka es un instrumento de origen arabe, al igual que el riq que es más antiguo y al cual la darbuka imita en estilo.
     Las tres técnicas de darbuka más destacadas son dos arabes y la turca, algunos interpretes combinan las tecnicas.
-    La primera y segunda técnicas tradicionales arabes se diferencian solamente en la mano izquierda, mientras que en la primera se utiliza sólo el dedo anular, en la segunda se utilizan los dedos anular, corazón e indice.
+    Las dos árabes se diferencian sólo en la mano izquierda. En la primera se utiliza sólo el dedo anular, en la segunda se utilizan los dedos anular, corazón e índice.
     La turca es muy diferente, utilizan combinaciones con los dedos anular e indice de ambas manos. 
     Faizal Zedan explica un rudimento llamado Qarshi, que es raro no encontrar en cualquier composición para darbuka, explicandolo en las tres diferentes tecnicas: `,
 }
