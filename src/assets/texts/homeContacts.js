b97f98390d257877e145ef1f19b78cae3668cee0
CasioPT87 <sergioibanezmoreno@gmail.com>
2019-09-30 19:36:59 +0100

    Ritmos estilizados

--- src/assets/texts/homeContacts.js
+++ src/assets/texts/homeContacts.js
@@ -3,14 +3,10 @@
 
 export default {
     text_1: `Cada estilo de música tiene sus ritmos caracteristicos aunque a veces coinciden por ejemplo ritmos muy generalizados como el vals, rumba o ritmos cortos que se utilizan en muchas culturas diferentes y en dónde se encontro primero es una incognita.
-
     Una de las razones para que reconozcamos un estilo musical, es la ornamentación, en percusión se llaman rudimentos algunos ejemplos son flangs, diddle, paradiddle.
     La darbuka es un instrumento de origen arabe, al igual que el riq que es más antiguo y al cual la darbuka imita en estilo.
-    
     Las tres técnicas de darbuka más destacadas son dos arabes y la turca, algunos interpretes combinan las tecnicas.
-    
     La primera y segunda técnicas tradicionales arabes se diferencian solamente en la mano izquierda, mientras que en la primera se utiliza sólo el dedo anular, en la segunda se utilizan los dedos anular, corazón e indice.
-    La turca es muy diferente, utilizan combinaciones con los dedos anular e indice de ambas manos.
-    
+    La turca es muy diferente, utilizan combinaciones con los dedos anular e indice de ambas manos. 
     Faizal Zedan explica un rudimento llamado Qarshi, que es raro no encontrar en cualquier composición para darbuka, explicandolo en las tres diferentes tecnicas: `,
 }
