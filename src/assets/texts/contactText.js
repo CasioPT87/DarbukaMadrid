c12571714d0570e58861b50028d22f8548a9e31b
CasioPT87 <sergioibanezmoreno@gmail.com>
2019-10-30 20:54:00 +0000

    fixed typo in text

--- src/assets/texts/contactText.js
+++ src/assets/texts/contactText.js
@@ -4,5 +4,5 @@
 export default {
     text_1: `
     Sara Moreno, formada en Darbuka con varios profesores, tales como Hector "El Turko", Luís Taberna y Al Halabi en Madrid y Mohamed Awami en Tánger.
-    Formada en Tabla India por profesores y musicos localos en Baranashi y Delhi, por Bakuji Shivastar en Londres y con Nanta Kumar en Madrid.`
+    Formada en Tabla India por profesores y musicos locales en Baranashi y Delhi, por Bakuji Shivastar en Londres y con Nanta Kumar en Madrid.`
 }  
