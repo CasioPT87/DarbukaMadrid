7e7655ac7248a4a9d70fabf5cf8b3710ee8fddbb
CasioPT87 <sergioibanezmoreno@gmail.com>
2019-10-30 20:52:37 +0000

    now the text is removed indeed

--- src/assets/texts/contactText.js
+++ src/assets/texts/contactText.js
@@ -4,6 +4,5 @@
 export default {
     text_1: `
     Sara Moreno, formada en Darbuka con varios profesores, tales como Hector "El Turko", Luís Taberna y Al Halabi en Madrid y Mohamed Awami en Tánger.
-    Estudió Flauta Clásica y jazz con Nacho Vallejo en Madrid.
     Formada en Tabla India por profesores y musicos localos en Baranashi y Delhi, por Bakuji Shivastar en Londres y con Nanta Kumar en Madrid.`
 }  
